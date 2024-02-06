aeb2a020b05897f38680480d92b56662e10ee7b8
MG <89721794+MinGi-SUNG@users.noreply.github.com>
2024-02-06 17:50:45 +0900

    [ADD/PASS] BOJ 2160

--- week01/BOJ 2160/BOJ_2160_성민기.java
+++ week01/BOJ 2160/BOJ_2160_성민기.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 //import java.util.StringTokenizer;
 
+
 public class BOJ_2160_성민기 {
 	public static void main(String[] args) throws IOException {
 //		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
