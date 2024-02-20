3f14eb740b0c01c4ea9743a07b9ca2219e1df5a4
joareum <a_lme@naver.com>
2024-02-20 14:07:35 +0900

    블랙잭

---
+++ week03/BOJ 2798 블랙잭/BOJ_2798_조아름.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2798_조아름 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = 0;
+		for(int i=0;i<N-2;i++) {
+			for(int j=i+1;j<N-1;j++) {
+				for(int k=j+1;k<N;k++) {
+					int sum = arr[i]+arr[j]+arr[k];
+					if(sum<=M) {
+						max = Math.max(max, sum);
+					}
+				}
+			}
+		}
+		
+		System.out.println(max);
+		
+	}
+
+}
