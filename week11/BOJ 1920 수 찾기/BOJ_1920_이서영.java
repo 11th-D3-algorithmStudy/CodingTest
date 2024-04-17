cb3084880fea761d0f41d1ac4d38d3194044bda5
Anhaechan <hcahna789@naver.com>
2024-04-17 16:51:02 +0900

    [ADD/PASS] BOJ 1920 수 찾기

---
+++ week11/BOJ 1920 수 찾기/BOJ_1920_이서영.java
@@ -0,0 +1,31 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1920_이서영 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] seq = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(seq);
+
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (Arrays.binarySearch(seq, num) < 0) {
+				System.out.println(0);
+			} else {
+				System.out.println(1);
+			}
+		}
+	}
+}
